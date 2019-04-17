dee0d5fdd921885ce9db666f158fb91b8a9b3fd7
Filipe Soares <filipecarlossoares@gmail.com>
2019-04-17 12:45:31 -0300

    Create SignUp page and add firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+  // Initialize Firebase
+var config = {
+  
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login'
+import SignUp from './views/SignUp'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/registrar',
+      name: 'signUp',
+      component: SignUp
     }
   ]
 })
